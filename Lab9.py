5105c81f7362e07199de21ce390c9cdb90dbc225
chrisv051904 <chrisv051904@gmail.com>
2024-04-10 21:02:59 -0400

    Fixed main

--- Lab9.py
+++ Lab9.py
@@ -7,15 +7,19 @@ def encode(password):
     return encoded_password
 
 # Decoder function
-def decode(encoded_password):
-    decoded_password = ""
-    for digit in encoded_password:
-        decoded_digit = str((int(digit) - 3) % 10)  # Shift each digit down by 3 numbers
-        decoded_password += decoded_digit
-    return decoded_password
-
+# Christian Villa
+def decode(s):
+    # Takes string s
+    t = ""
+    for i in range(0, len(s)):
+        x = int(s[i])
+        #using -3 = 7 mod 10
+        x = (x + 7) % 10
+        t += str(x)
+    return t
 # Main function
 def main():
+    encoded_password = ""
     while True:
         print("Menu\n-------------")
         print("1. Encode")
@@ -28,7 +32,6 @@ def main():
             encoded_password = encode(password)
             print("Your password has been encoded and stored!")
         elif option == "2":
-            encoded_password = input("Please enter the encoded password: ")
             decoded_password = decode(encoded_password)
             print(f"The encoded password is {encoded_password}, and the original password is {decoded_password}.")
         elif option == "3":
